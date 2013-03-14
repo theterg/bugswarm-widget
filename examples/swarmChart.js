376869552e6029d257ebc8d848899aa10557bb23
Andrew Tergis <theterg@gmail.com>
2013-03-14 18:45:04 -0400

    new widget - swarmChart
    Right now it implies the 'Acceleration' service.  Need to make it more
    flexible.

---
+++ examples/swarmChart.js
@@ -0,0 +1,145 @@
+//TODO - auto-instantiate jquery if not found!
+
+(function($, undefined) {
+
+	//name of the Widget namespace within jQuery.
+	var namespace = 'bug.swarmChart';
+
+	//an object representing this widget's instance scope
+	//this is copied from the widget's scope on create
+	var my = {};
+
+	//Default options for the widget
+	//Anything passed into $('div').widget({...}) will override this.
+	var options = {
+		swarm: false,					//swarm session object (SWARM)
+		resource: false,				//Resource to follow
+		printWholePayload: false,		//print whole payload, or only a feed?
+		feed: false,					//What feed should we display?
+		debug: true,					//print debug messages to console
+		value: 0,
+		width: 450,
+		height: 250,
+		plotlen: 100,
+		chart: {
+				series: { shadowSize: 0 }, // drawing is faster without shadows
+				grid: { color: "#FFF" },
+				legend: { backgroundColor: "#5C5D60" },
+				yaxis: { position: "left",
+					min: -1.5,
+					max: 1.5 }
+			}
+	};
+
+	var info = function(message) {
+		console.log('['+my.eventNamespace+']: '+message);
+	};
+	var debug = function(message) {
+		if (options.debug)
+			console.log('['+my.eventNamespace+']: '+message);
+	};
+
+	//Invoked when widget is first attached to an element
+	//add classes, store references, create elements, initialize other widgets
+	var create = function() {
+		my = this;						//Assign this widget's scope to 'my'
+		debug('create');
+		var el = my.element.hide();
+		var opts = my.options;
+
+		//Check for mandatory options
+		if (!my.options.swarm) {
+			info('ERR: must specify swarm creating widget');
+		}
+		$(el).css('width',my.options.width).css('height',my.options.height);
+		my.accelX = [];
+		my.accelY = [];
+		my.accelZ = [];
+		my.plot = $.plot(el,
+			[my.accelX, my.accelY, my.accelZ],
+			my.options.chart);
+	};
+
+	var onSwarmMessage = function(message) {
+		//The 'my' context should have been set on this callback...
+		var my = this;
+		//debug(my.options.feed);
+		var payload  = message.payload;
+		if (my.options.resource && 
+			(message.from.resource !== my.options.resource)) {
+			return;
+		}
+		if (my.options.printWholePayload){
+			update(JSON.stringify(payload));
+		} else {
+			if (!('feed' in payload)) { return; }
+			if (!my.options.feed){
+				update(JSON.stringify(payload));
+			} else if (payload.name === my.options.feed) {
+				//console.log(my);
+				update.apply(my, [payload.feed]);
+				//update(JSON.stringify(payload.feed));
+			}
+		}
+	};
+
+	//called after _create, every time widget is re-initialized
+	//more complex initialization
+	var init = function() {
+		debug('init');
+		update('Waiting for data...');
+		my.options.swarm.addListener('message', onSwarmMessage, my);
+		my.element.show();
+		my.startTime = (new Date()).getTime();
+	};
+
+	//Called when widget is detached from element
+	//should re-set element to how it was before we were there
+	var destroy = function() {
+		debug('destroy');
+	};
+
+	//Only update the value of this widget, avoid a full redraw.
+	var update = function(data) {
+		debug('update '+JSON.stringify(data));
+		//console.log(this);
+		if (this !== window){
+			my = this;
+		}
+		if (data !== undefined) {
+			my.options.value = data;
+		}
+		var currentTime = (new Date()).getTime();
+		my.accelX.push([(currentTime - my.startTime)/1000, data.x]);
+		my.accelY.push([(currentTime - my.startTime)/1000, data.y]);
+		my.accelZ.push([(currentTime - my.startTime)/1000, data.z]);
+		if (my.accelX.length > my.plotlen){
+            my.accelX.shift();
+            my.accelY.shift();
+            my.accelZ.shift();
+        }
+		my.plot = $.plot(my.element,
+			[my.accelX, my.accelY, my.accelZ],
+			my.options.chart);
+	};
+
+	//Called when a user changes an option, after the variable has been set.
+	//Should re-initialize parts of the widget that depend on the option
+	var optionChanged = function(name, value) {
+		debug(name+' changed to '+JSON.stringify(value));
+		my.options[name] = value;
+		update();
+	};
+
+
+	$.widget(namespace, {
+		// Begin jQuery-specific objects/functions: 
+		_create: create,
+		_init: init,
+		destroy: destroy,
+		_setOption: optionChanged,
+		options: options,
+		// End jQuery-specific options/functions
+		update: update
+	});
+}(jQuery));
